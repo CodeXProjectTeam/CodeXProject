c5fdc81e30257b85b1186fe13acb851ec34531e8
高凯 <kxjhaha@gmail.com>
2016-12-13 21:43:49 +0800

    fc

---
+++ Assets/Script/Core/Lua/LuaManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using LuaInterface;
+using System;
+
+public class LuaManager  
+{
+    static LuaState state = new LuaState();
+
+    public static void Init()
+    {
+        try 
+        {
+            state.Start();
+            //LuaBinder.Bind(state);
+
+            state.DoString("print('hello world')","test.lua");
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e.ToString());
+        }
+
+        ApplicationManager.s_OnApplicationUpdate += Update;   
+    }
+
+    static void Update()
+    {
+
+    }
+
+    public static void DoLuaFile(string fileName)
+    {
+        string content = ResourceManager.ReadTextFile(fileName);
+        state.DoString(content, fileName);
+    }
+}
